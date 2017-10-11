22df224b3942a93511a466af948613d926afcd5e
pavkoo <pavkoo@live.com>
2017-10-11 16:47:22 +0800

    update project to android studio

---
+++ franklinHD/src/main/java/com/pavkoo/franklin/controls/SettingSystemMeDialog.java
@@ -0,0 +1,81 @@
+package com.pavkoo.franklin.controls;
+
+import com.pavkoo.franklin.R;
+import com.pavkoo.franklin.controls.AnimMessage.AnimMessageType;
+import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
+import com.umeng.update.UpdateStatus;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SettingSystemMeDialog extends ParentDialog {
+	private AnimationDrawable ad;
+	private ImageView ivbg;
+	private TextView settingmeCheckupdate;
+	private TextView tvcopyright;
+	private AnimMessage amMessage;
+	
+	public SettingSystemMeDialog(Context context, int theme) {
+		super(context, theme);
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+		View dialogView = li.inflate(R.layout.settting_system_popup_me, null);
+		ivbg = (ImageView) dialogView.findViewById(R.id.ivSettingSystemMe);
+		settingmeCheckupdate = (TextView) dialogView.findViewById(R.id.settingmeCheckupdate);
+		tvcopyright = (TextView) dialogView.findViewById(R.id.tvcopyright);
+		amMessage = (AnimMessage) dialogView.findViewById(R.id.amSettingsystemMeMessage);
+		setContentView(dialogView);		
+		ad = (AnimationDrawable) ivbg.getDrawable();
+		ad.start();
+		
+		tvcopyright.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.cnblogs.com/pavkoo/p/4102992.html"));
+				try {
+					SettingSystemMeDialog.this.getContext().startActivity(browserIntent);
+				} catch (ActivityNotFoundException e) {
+					amMessage.showMessage(SettingSystemMeDialog.this.getContext().getString(R.string.cantFindBrowse), AnimMessageType.ERROR);
+				}
+			}
+		});
+		settingmeCheckupdate.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				UmengUpdateAgent.setUpdateAutoPopup(false);
+				UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+				    @Override
+				    public void onUpdateReturned(int updateStatus,UpdateResponse updateInfo) {
+				        switch (updateStatus) {
+				        case UpdateStatus.Yes: // has update
+				            UmengUpdateAgent.showUpdateDialog(SettingSystemMeDialog.this.getContext(), updateInfo);
+				            break;
+				        case UpdateStatus.No: // has no update
+				        	amMessage.showMessage(getContext().getString(R.string.no_update));
+				            break;
+				        case UpdateStatus.NoneWifi: // none wifi
+				        	amMessage.showMessage(getContext().getString(R.string.no_wifi),AnimMessageType.ERROR);
+				            break;
+				        case UpdateStatus.Timeout: // time out
+				        	amMessage.showMessage(getContext().getString(R.string.timeout),AnimMessageType.ERROR);
+				            break;
+				        }
+				    }
+				});
+				UmengUpdateAgent.forceUpdate(SettingSystemMeDialog.this.getContext());
+				amMessage.showMessage(getContext().getString(R.string.checking_now),AnimMessageType.Waitting);
+			}
+		});
+	}
+}
